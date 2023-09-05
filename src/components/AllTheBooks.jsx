7ca0d4e1bf2622036d6d986fe74e07631c87efa7
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-09-05 17:30:21 +0200

    update file

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -7,13 +7,15 @@ import { Component } from "react";
 
 class AllTheBooks extends Component {
    render(){
+    
     return (
         <Container fluid>
             <Row className='row-cols-2 row-cols-md-3 row-cols-md-4 gy-3'>
                 {fantasy.map((book, index) => (
-                    <Col>
-                        <Book src={book.img} title={book.title} key={index} />
+                    <Col key={`book-${index}`}>
+                        <Book src={book.img} title={book.title}  />
                     </Col>
+                 
                 ))}
             </Row>
         </Container>
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -6,7 +6,7 @@ class Book extends Component{
 
     render(){
         return( 
-                <Card className="h-100">
+                <Card className="h-100" >
                     <Card.Img variant="top" src={this.props.src} className="h-100 object-fit-cover" />
                     <Card.Body className="d-flex">
                         <Card.Title className="mt-auto"> {this.props.title}$</Card.Title>
